67e0e663dda77a1471ee860b8ea3708db1f34aa7
Konrad Misterski <kmisterski@o2.pl>
2024-09-29 17:37:43 +0200

    Added other shapes

--- include/tetris/impl/Shapes.hpp
+++ include/tetris/impl/Shapes.hpp
@@ -5,7 +5,10 @@
 namespace tetris::impl
 {
 
-//TODO add other shapes
 extern ShapeBlocks LShape;
+extern ShapeBlocks TShape;
+extern ShapeBlocks IShape;
+extern ShapeBlocks ZShape;
+extern ShapeBlocks BoxShape;
 
 }  // namespace tetris::impl
--- src/impl/Shapes.cpp
+++ src/impl/Shapes.cpp
@@ -4,5 +4,9 @@ namespace tetris::impl
 {
 
 ShapeBlocks LShape = {{{0, 0}, {0, 1}, {0, 2}, {1, 2}}};
+ShapeBlocks TShape = {{{0, 0}, {0, 1}, {0, 2}, {1, 1}}};
+ShapeBlocks IShape = {{{0, 0}, {1, 0}, {2, 0}, {3, 0}}};
+ShapeBlocks ZShape = {{{0, 0}, {1, 0}, {1, 1}, {1, 2}}};
+ShapeBlocks BoxShape = {{{0, 0}, {0, 1}, {1, 0}, {1, 1}}};
 
 }  // namespace tetris::impl
